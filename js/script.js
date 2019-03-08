1e5d3237a319457aab233ddf97afad2fdb1727c4
Nam <tdnam39@gmail.com>
2019-03-08 13:17:33 +1100

    add auto type job title

--- js/script.js
+++ js/script.js
@@ -4,4 +4,17 @@ $(document).ready(() => {
     play: 5000
     // pagination: false
   })
+
+  let typed = new Typed('.typed', {
+    strings: [
+      '...',
+      'Junior Web Developer',
+      'Junior Software Developer',
+      'Technology Hobbyist'
+    ],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
+  })
 })
