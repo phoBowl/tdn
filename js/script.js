1f110d04d0af8e9a068fa41dad6002c0297c27d9
Nam <tdnam39@gmail.com>
2019-03-08 14:16:47 +1100

    add pie chart for skills
    * percentage
    * using easypiechart

--- js/script.js
+++ js/script.js
@@ -17,21 +17,39 @@ $(document).ready(() => {
     startDelay: 1000,
     showCursor: false
   })
-})
 
-$('.owl-carousel').owlCarousel({
-  loop: true,
-  margin: 10,
-  nav: true,
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 3
-    },
-    1000: {
-      items: 5
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    // margin: 10,
+    // nav: true,
+    items: 4,
+    responsive: {
+      0: {
+        items: 1
+      },
+      480: {
+        items: 2
+      },
+      768: {
+        items: 3
+      },
+      938: {
+        items: 4
+      }
     }
-  }
+  })
+
+  $('.chart').easyPieChart({
+    easing: 'easeInOut',
+    barColor: '#fff',
+    trackColor: false,
+    scaleColor: false,
+    lineWidth: 4,
+    size: 152,
+    onStep: function(from, to, percent) {
+      $(this.el)
+        .find('.percent')
+        .text(Math.round(percent))
+    }
+  })
 })
