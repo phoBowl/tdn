3d141e834cc01060c80202487dd72cbec3f15c0e
Nam <tdnam39@gmail.com>
2019-03-10 18:09:13 +1100

    sticky nav bar

--- js/script.js
+++ js/script.js
@@ -101,4 +101,18 @@ $(document).ready(function() {
 
     return false
   })
+
+  const nav = $('#navigation')
+  const navTop = nav.offset().top
+
+  $(window).on('scroll', stickyNavigation)
+
+  function stickyNavigation() {
+    var body = $('body')
+    if ($(window).scrollTop() >= navTop) {
+      body.addClass('fixedNav')
+    } else {
+      body.removeClass('fixedNav')
+    }
+  }
 })
