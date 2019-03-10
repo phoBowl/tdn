cfd45a79455b84ce5cbd631132dfd4e9197a609c
Nam <tdnam39@gmail.com>
2019-03-10 11:06:20 +1100

    styling items

--- js/script.js
+++ js/script.js
@@ -72,4 +72,14 @@ $(document).ready(() => {
       countUpFinished = true
     }
   })
+
+  $('[data-fancybox]').fancybox()
+
+  $('.items').isotope({
+    filter: '*',
+    animationOptions: {
+      duration: 1500,
+      easing: 'linear'
+    }
+  })
 })
